17c51b28d3ec196249ea968a7890d7ee954bb34b
Rainer Simon <rainer.simon@ait.ac.at>
2020-04-20 07:51:50 +0200

    First data filtering step

---
+++ preprocessing/filter-columns.py
@@ -0,0 +1,25 @@
+"""
+Filters the relevant columns from the soure CSV, so that we get 
+something readable.
+"""
+import pandas as pd
+
+INPUT_FILE = '../data/TravelogueD16_2020-03-13.csv'
+OUTPUT_FILE = '../data/TravelogueD16_filtered.csv'
+
+data = pd.read_csv(INPUT_FILE, delim_whitespace=False, sep=',', quotechar='"', index_col=0)
+# print(data.iloc[17]) # Output a sample record
+
+filtered = data[[
+  'Barcode', 
+  'Verlagsort normiert ; GND-ID',
+  'Verleger normiert ; GND-ID',
+  'Drucker ; GND-ID',
+  'Erscheinungsjahr',
+  'Marker',
+  'Volltext',
+  'Werktitel',
+  'Haupttitel ; Titelzusatz ; Verantwortlichkeitsangabe'
+]]
+
+filtered.to_csv(OUTPUT_FILE, index=False)
