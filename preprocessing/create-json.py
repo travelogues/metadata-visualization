1f9f690c17a2864938c6fc5da9db31bd4b302bf1
Rainer Simon <rainer.simon@ait.ac.at>
2020-04-21 12:33:16 +0200

    Data cleaning tweaks

--- preprocessing/create-json.py
+++ preprocessing/create-json.py
@@ -5,9 +5,12 @@ import re
 INPUT_FILE = '../data/TravelogueD16_filtered.csv'
 OUTPUT_FILE = '../data/TravelogueD16.json'
 
-def parse_gnd_field(field):
-  tokens = field.split(';')
-  return list(filter(lambda t: t.strip().startswith('(DE-') == False and len(t.strip()) > 0, tokens))
+def parse_gnd_field(field, default = 'Unknown'):
+  if (field):
+    tokens = field.split(';')
+    return list(filter(lambda t: t.strip().startswith('(DE-') == False and len(t.strip()) > 0, tokens))
+  else:
+    return [ default ]
 
 def parse_date(field):
   numbers_only = re.sub('[^0-9]', '', field)
@@ -23,6 +26,15 @@ def parse_markers(field):
   tokens = list(map(lambda str: str.split(' '), tokens))
   return [y for x in tokens for y in x]
 
+def parse_nullable(field, default, split):
+  if (field):
+    if (split):
+      return [ str.strip() for str in field.split(';') ]
+    else:
+      return field.strip()
+  else:
+    return default
+
 with open(INPUT_FILE, 'r') as infile, open(OUTPUT_FILE, 'w') as outfile:
   reader = csv.reader(infile)
 
@@ -31,17 +43,26 @@ with open(INPUT_FILE, 'r') as infile, open(OUTPUT_FILE, 'w') as outfile:
   as_json = []
   
   for row in reader:
+    publishers = parse_gnd_field(row[4])
+    printers = parse_gnd_field(row[5])
+
+    # For convenience
+    people = list(set(publishers + printers))
+    if (len(people) > 1):
+      people = [ p for p in people if p != 'Unknown' ]
+    
     as_json.append({
-      'barcode': row[0],
-      'url': row[6],
-      'place_of_publication': parse_gnd_field(row[1])[0],
-      'publishers': parse_gnd_field(row[2]),
-      'printers': parse_gnd_field(row[3]),
-      'people': list(set(parse_gnd_field(row[3]) + parse_gnd_field(row[2]))), # for convenience
-      'date': parse_date(row[4]),
-      'marker_regions': parse_markers(row[5]),
-      'work_title': row[7],
-      'title_full': row[8]
+      'identifier': row[0],
+      'barcodes': parse_nullable(row[1], [], True),
+      'urls': parse_nullable(row[2], [], True),
+      'place_of_publication': parse_gnd_field(row[3])[0],
+      'publishers': publishers,
+      'printers': printers,
+      'people': people,
+      'date': parse_date(row[6]),
+      'marker_regions': parse_markers(row[7]),
+      'work_title': row[8],
+      'title_full': row[9]
     })
 
   outfile.write(json.dumps(as_json, indent=2))
--- preprocessing/filter-columns.py
+++ preprocessing/filter-columns.py
@@ -10,18 +10,19 @@ OUTPUT_FILE = '../data/TravelogueD16_filtered.csv'
 data = pd.read_csv(INPUT_FILE, delim_whitespace=False, sep=',', quotechar='"', index_col=0)
 # print(data.iloc[17]) # Output a sample record
 
-data = data[data['Barcode'].notna()] # Remove rows without barcode
-data = data[~data['Barcode'].str.startswith('Z') == False] # Remove non-barcode barcodes (argh)
-data = data[~data['Barcode'].str.contains(';')] # Remove multiple barcode columns
+# data = data[data['Barcode'].notna()] # Remove rows without barcode
+# data = data[data['Bar~data['Barcode'].str.startswith('Z') == False] # Remove non-barcode barcodes (argh)
+# data = data[~data['Barcode'].str.contains(';')] # Remove multiple barcode columns
 
 filtered = data[[
+  'Systemnummer',
   'Barcode', 
+  'Volltext',
   'Verlagsort normiert ; GND-ID',
   'Verleger normiert ; GND-ID',
   'Drucker ; GND-ID',
   'Erscheinungsjahr',
   'Marker',
-  'Volltext',
   'Werktitel',
   'Haupttitel ; Titelzusatz ; Verantwortlichkeitsangabe'
 ]]
